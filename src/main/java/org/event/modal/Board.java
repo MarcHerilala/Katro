d00fb6eccbc67ae97dd234ee6f525af79bcbceb6
Marc985 <hei.marc.3@gmail.com>
2025-03-01 10:51:44 +0300

    chore:add players on the board

--- src/main/java/org/event/modal/Board.java
+++ src/main/java/org/event/modal/Board.java
@@ -1,10 +1,12 @@
 package org.event.modal;
 
 import org.event.modal.player.Player;
+import org.event.modal.player.Player1;
+import org.event.modal.player.Player2;
 
 public class Board {
-    private Player topPlayer;
-    private Player bottomPlayer;
+    Player1 player1;
+    Player2 player2;
 
 
 }
